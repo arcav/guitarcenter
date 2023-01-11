38698cf94d5fceebcb33cf6b34cb33057663947d
arcav <armandojcastro84@gmail.com>
2023-01-10 22:10:34 -0400

    feature:basic navigation distribution

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,15 @@
 const NavBar = () => {
-  return <nav className="w-full h-10 bg-red-500">Navbar</nav>;
+  return (
+    <nav className="flex items-center text-white w-full h-10 bg-slate-800">
+      <div className="w-1/2 px-3">
+        <div className=" bg-red-400 ">Logo</div>
+      </div>
+      <div className="w-1/2 flex justify-evenly">
+        <div className=" bg-green-500 ">NavLink1</div>
+        <div className=" bg-green-500 ">NavLink1</div>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
