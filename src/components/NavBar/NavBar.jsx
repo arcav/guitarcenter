c287a2a6cafed535e129a3ff2eff9ae7edbf5c1c
arcav <armandojcastro84@gmail.com>
2023-01-11 22:50:35 -0400

    navbar prototype

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,20 @@
 const NavBar = () => {
   return (
-    <nav className="flex items-center text-white w-full h-10 bg-slate-800">
-      <div className="w-1/2 px-3">
-        <div className=" bg-red-400 ">Logo</div>
-      </div>
-      <div className="w-1/2 flex justify-evenly">
-        <div className=" bg-green-500 ">NavLink1</div>
-        <div className=" bg-green-500 ">NavLink1</div>
+    <nav className="fixed top-0 left-0 z-20 w-full bg-slate-500">
+      <div className="container mx-auto flex items-center justify-between py-4 px-2">
+        <div className="flex items-center gap-2">
+          <div className="rounded-full w-8 h-8 bg-red-400 flex items-center justify-center text-white font-bold ">
+            A
+          </div>
+          <p className="text-xl">
+            Guitar <span className="opacity-80">Center</span>
+          </p>
+        </div>
+        <div className="flex text-white w-1/3 justify-between gap-4">
+          <p>Links</p>
+          <p>Links</p>
+          <p>Links</p>
+        </div>
       </div>
     </nav>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [  "./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        heroShadow: "0px 0px 16px rgba(0,0,0,0.15)",
+      },
+    },
+    screens: {
+      xs: "320px",
+      sm: "768px",
+      md: "1060px",
+    },
   },
   plugins: [],
-}
+};
